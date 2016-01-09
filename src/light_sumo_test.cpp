57cdbb046a8d1eb605fe84b131e254c913f2da86
Arnaud Ramey <arnaud.a.ramey@gmail.com>
2016-01-09 12:03:41 +0100

    first ROS wrapper

--- src/light_sumo_test.cpp
+++ src/light_sumo_test.cpp
@@ -1,8 +1,8 @@
 /*!
-  \file        rossumo.cpp
+  \file        light_sumo_test.cpp
   \author      Arnaud Ramey <arnaud.a.ramey@gmail.com>
                 -- Robotics Lab, University Carlos III of Madrid
-  \date        2016/1/8
+  \date        2016/1/9
 
 ________________________________________________________________________________
 
@@ -19,44 +19,48 @@ Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ________________________________________________________________________________
+
+A simple test for the LightSumo class.
  */
 #include <rossumo/light_sumo.h>
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
 
-int main (int argc, char **argv) {
-  ros::init(argc, argv, "rossumo");
-  ros::NodeHandle nh_public;
-  // create publishers and subscribers
-  image_transport::ImageTransport it(nh_public);
-  image_transport::Publisher rgb_pub = it.advertise("rgb", 1);
-  // create and connect robot
+int main (/*int argc, char **argv*/) {
   LightSumo sumo;
+  cv::namedWindow("sumo");
   if (!sumo.connect())
     exit(-1);
-  // data
-  cv_bridge::CvImage rgb;
-  rgb.encoding = "bgr8";
+  cv::Mat rgb;
+
   unsigned int last_pix_idx = -1;
-  // data acquisition loop
-  ros::Rate rate(100);
-  while(ros::ok()) {
-    ros::Time now = ros::Time::now();
+  while(true) {
+
     if (sumo.get_pic_idx() != last_pix_idx) {
-      last_pix_idx = sumo.get_pic_idx();
-      sumo.get_pic(rgb.image);
-      if (rgb.image.empty())
+      sumo.get_pic(rgb);
+      if (rgb.empty())
         printf("pic empty!\n");
-      else {
-        // publish pic
-        rgb.header.stamp = now;
-        rgb.header.frame_id = "sumo_camera_frame";
-        rgb_pub.publish(rgb.toImageMsg());
-      }
+      else
+        cv::imshow("sumo", rgb);
+      last_pix_idx = sumo.get_pic_idx();
     }
-    ros::spinOnce();
-    rate.sleep();
-  } // end while (ros::ok())
+    char c = cv::waitKey(50);
+    // Commands in Sources/ARCONTROLLER_Feature.h - line 1405
+    if (c == 'i')  // go forward
+      sumo.set_speeds(50, 0);
+    else if (c == 'u')  // go diagonal
+      sumo.set_speeds(50, -0.05);
+    else if (c == 'o') // go diagonal
+      sumo.set_speeds(50, 0.05);
+    else if (c == 'k')  // go backwards
+      sumo.set_speeds(-50, 0);
+    else if (c == 'j')  // turn left on place
+      sumo.set_speeds(0, -0.05);
+    else if (c == 'l')  // turn left on place
+      sumo.set_speeds(0, 0.05);
+    else if (c == 'q' || c == 27)  //ESC
+      break;
+    else  // stop robot
+      sumo.set_speeds(0, 0);
+  }
   return EXIT_SUCCESS;
 } // end main()
+
