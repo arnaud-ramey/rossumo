5126b02313186d332e1ca61e8d258ea213429765
Arnaud Ramey <arnaud.a.ramey@gmail.com>
2016-01-21 23:19:58 +0100

    visual odom

--- src/rossumo.cpp
+++ src/rossumo.cpp
@@ -63,7 +63,7 @@ ________________________________________________________________________________
 \section Publications
   - \b "camera/image_raw"
     [sensor_msgs::Image]
-    The 640x480 RGB image, encoded as "bgr8".
+    The 640x480 raw image, encoded as "bgr8".
     The framerate is roughly 15 fps.
     The image comes from the MJPEG video stream of the robot
     and is not decoded if there is no subscriber to the topic.
@@ -106,29 +106,25 @@ ________________________________________________________________________________
 #include <std_msgs/Int16.h>
 #include <std_msgs/String.h>
 #include <camera_calibration_parsers/parse.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 
 class RosSumo : public LightSumo {
 public:
   RosSumo() : _nh_private("~") {
     // initial data
-    _rgb.encoding = "bgr8";
-    _rgb.header.frame_id = "sumo_camera_frame";
-    _caminfo.header = _rgb.header;
+    _image_rect.encoding = _image_raw.encoding = "bgr8";
+    _image_rect.header.frame_id = _image_raw.header.frame_id = "/camera";
+    _caminfo.header = _image_raw.header;
     // read params
     std::string cal_filename = "", cal_camname = "camname";
     _caminfo_read = false;
     _nh_private.param("camera_calibration_filename", cal_filename, cal_filename);
     _nh_private.param("camera_calibration_camname", cal_camname, cal_camname);
-    if (!cal_filename.empty()) {
-      _caminfo_read = camera_calibration_parsers::readCalibration
-                      (cal_filename, cal_camname, _caminfo);
-      if (!_caminfo_read)
-        ROS_WARN("Could not read camera '%s' in camera_calibration file '%s'",
-                 cal_camname.c_str(), cal_filename.c_str());
-      else
-        ROS_INFO("Succesfully read camera '%s' in camera_calibration file '%s'",
-                 cal_camname.c_str(), cal_filename.c_str());
-    }
+    if (!cal_filename.empty())
+      read_calibration(cal_filename, cal_camname);
+
     // create publishers
     _it = new image_transport::ImageTransport(_nh_public);
     _cmd_vel_sub = _nh_public.subscribe("cmd_vel", 1, &RosSumo::cmd_vel_cb, this);
@@ -138,7 +134,8 @@ public:
     _high_jump_sub = _nh_public.subscribe("high_jump", 1, &RosSumo::high_jump_cb, this);
     _long_jump_sub = _nh_public.subscribe("long_jump", 1, &RosSumo::long_jump_cb, this);
     // create subscribers
-    _rgb_pub = _it->advertise("camera/image_raw", 1);
+    _image_raw_pub = _it->advertise("camera/image_raw", 1);
+    _image_rect_pub = _it->advertise("camera/image_rect_color", 1);
     _caminfo_pub = _nh_public.advertise<sensor_msgs::CameraInfo>("camera/camera_info", 1);
     _battery_percentage_pub = _nh_public.advertise<std_msgs::Int16>("battery_percentage", 1);
     _posture_pub = _nh_public.advertise<std_msgs::String>("posture", 1);
@@ -149,12 +146,47 @@ public:
 
   //////////////////////////////////////////////////////////////////////////////
 
+  bool read_calibration(std::string & cal_filename, std::string & cal_camname) {
+    _caminfo_read = camera_calibration_parsers::readCalibration
+        (cal_filename, cal_camname, _caminfo);
+    if (!_caminfo_read) {
+      ROS_WARN("Could not read camera '%s' in camera_calibration file '%s'",
+               cal_camname.c_str(), cal_filename.c_str());
+      return false;
+    }
+    // Read camera parameters from CamerInfo
+    //  printf("K:%i, D:%i\n", _caminfo.K.size(), _caminfo.D.size());
+    //  std::cout << "intrinsics:" << intrinsics << std::endl;
+    //  std::cout << "distortion:" << distortion << std::endl;
+    intrinsics.create(3, 3, CV_32FC1);
+    for (int row = 0; row < 3; ++row)
+      for (int col = 0; col < 3; ++col)
+        intrinsics.at<float>(row, col) = _caminfo.K[3*row+col];
+    distortion.create(5, 1, CV_32FC1); // rows, cols
+    for (int col = 0; col < 5; ++col)
+      distortion.at<float>(col, 0) = _caminfo.D[col];
+    // Computes the undistortion and rectification transformation map.
+    // cf http://docs.opencv.org/trunk/modules/imgproc/doc/geometric_transformations.html#initundistortrectifymap
+    cv::Mat newCameraMatrix;
+    cv::initUndistortRectifyMap(intrinsics, distortion, cv::Mat(), newCameraMatrix,
+                                cv::Size(_caminfo.width, _caminfo.height), CV_32FC1,
+                                mapx, mapy);
+    intrinsics_inv = intrinsics.inv();
+    ROS_INFO("Succesfully read camera '%s' in camera_calibration file '%s'",
+             cal_camname.c_str(), cal_filename.c_str());
+    return true;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
   ~RosSumo() { delete _it; }
 
   //////////////////////////////////////////////////////////////////////////////
 
   void spinOnce() {
-    if (_rgb_pub.getNumSubscribers() || _caminfo_pub.getNumSubscribers())
+    if (_image_raw_pub.getNumSubscribers()
+        || _image_rect_pub.getNumSubscribers()
+        || _caminfo_pub.getNumSubscribers())
       enable_pic_decoding();
     else
       disable_pic_decoding();
@@ -188,15 +220,23 @@ protected:
 
   //! callback called when a new image is received
   virtual void imageChanged () {
-    get_pic(_rgb.image);
-    if (_rgb.image.empty()) {
+    get_pic(_image_raw.image);
+    if (_image_raw.image.empty()) {
       printf("pic empty!\n");
       return;
     }
-    // publish pic
-    _rgb.header.stamp = ros::Time::now();
-    _rgb_pub.publish(_rgb.toImageMsg());
-    _caminfo.header.stamp  = _rgb.header.stamp;
+    _image_raw.header.stamp = ros::Time::now();
+    _image_rect.header.stamp = _image_raw.header.stamp;
+    // rectify image if needed
+    if (_image_rect_pub.getNumSubscribers()) {
+      cv::remap(_image_raw.image, _image_rect.image, mapx, mapy, cv::INTER_LINEAR);
+      _image_rect_pub.publish(_image_rect.toImageMsg());
+    }
+    // publish raw pic
+    if (_image_raw_pub.getNumSubscribers()) {
+      _image_raw_pub.publish(_image_raw.toImageMsg());
+    }
+    _caminfo.header.stamp  = _image_raw.header.stamp;
     _caminfo_pub.publish(_caminfo);
   }
 
@@ -257,7 +297,7 @@ protected:
   //////////////////////////////////////////////////////////////////////////////
 
   image_transport::ImageTransport* _it;
-  image_transport::Publisher _rgb_pub;
+  image_transport::Publisher _image_raw_pub, _image_rect_pub;
   ros::Subscriber _cmd_vel_sub, _high_jump_sub, _long_jump_sub, _posture_sub;
   ros::Subscriber _anim_sub, _sharp_turn_sub;
   ros::Publisher _caminfo_pub, _volume_pub, _battery_percentage_pub, _posture_pub;
@@ -268,12 +308,16 @@ protected:
   ros::NodeHandle _nh_public, _nh_private;
   sensor_msgs::CameraInfo _caminfo;
   bool _caminfo_read;
-  cv_bridge::CvImage _rgb;
+  cv_bridge::CvImage _image_raw, _image_rect;
+  cv::Mat mapx, mapy;
+  cv::Mat intrinsics, distortion, intrinsics_inv;
 }; // end class RosSumo
 
 ////////////////////////////////////////////////////////////////////////////////
 
 int main (int argc, char **argv) {
+#if 1
+#endif
   ros::init(argc, argv, "rossumo");
   RosSumo sumo;
   if (!sumo.connect())
